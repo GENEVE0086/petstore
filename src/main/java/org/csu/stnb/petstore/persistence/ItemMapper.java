f1960982208a2257d37a9e9a684afc9fdb4c8c3d
詹 <2575773858@qq.com>
2019-06-12 18:39:11 +0800

    完成商品展示，搜索功能及帮助页面的跳转

---
+++ src/main/java/org/csu/stnb/petstore/persistence/ItemMapper.java
@@ -0,0 +1,17 @@
+package org.csu.stnb.petstore.persistence;
+
+import org.csu.stnb.petstore.domain.Item;
+
+import java.util.List;
+import java.util.Map;
+
+public interface ItemMapper {
+    void updateInventoryQuantity(Map<String,Object> param);
+
+    int getInventoryQuantity(String itemId);
+
+    List<Item> getItemListByProduct(String productId);
+
+    Item getItem(String itemId);
+}
+
